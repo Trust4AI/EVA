d1c6d6c31412cf228eb3ed5916a7a663ab13418b
Javier Vázquez Zambrano <javvazzam@gmail.com>
2025-02-27 21:41:01 +0100

    Adicion de parametro default para controlar el resultado de la evaluacion

--- src/evaluators/mc_evaluator.py
+++ src/evaluators/mc_evaluator.py
@@ -1,7 +1,7 @@
 import re
 
 
-def evaluate_mc(prompt, expected_result, generated_result):
+def evaluate_mc(prompt, expected_result, generated_result, default):
     options = _extract_options(prompt)
     # expected_result = _extract_options(expected_result)[0]
     options = [option.replace('.','').replace('?','') for option in options if option != expected_result.lower()]
@@ -15,7 +15,7 @@ def evaluate_mc(prompt, expected_result, generated_result):
     elif [option.replace(",", "").replace(".", "").strip().lower() in generated_result.replace(",", "").replace(".", "").strip().lower() for option in options].count(True) > 1:
         return 'fail'
     else:
-        return 'pass'
+        return default
 
 
 def _extract_options(prompt):
